538a86c316da2ce5ad1ab02788c6024decfd0896
uf-code <ugur.firat.developer@gmail.com>
2022-02-12 00:45:24 +0300

    read and insert into mysql database

--- python-mysql.py
+++ python-mysql.py
@@ -10,46 +10,36 @@ def read_everything(table_name):
     for item in cursor:
         print(item)
 
-    
 
+def insert_data(first_name, last_name, email):
+    query = f"INSERT INTO customers (first_name, last_name, email, birthdate, createdAt, updatedAt) VALUES('{first_name}', '{last_name}', '{email}', '{date.today()}', '{datetime.now()}', '{datetime.now()}')"
 
-# query = f"INSERT INTO customers (first_name, last_name, email, birthdate, createdAt, updatedAt) VALUES('UGUR', 'FIRAT', 'ugur@firat123.com', '1111-11-11', '{datetime.now()}', '{datetime.now()}')"
+    cursor.execute(query)
 
-# cursor.execute(query)
-
-
-# query = "SELECT * FROM customers"
-# cursor.execute(query)
-
-# for customer in cursor:
-#     print(customer)
-
-
-
-
-
-
-# print(datetime.now())
-# 2022-02-11 23:43:01.113319
 
 
 if __name__ == "__main__":    
     try:
         cnx = mysql.connector.connect(user='root', password='password',
-                                host='localhost',
-                                database='job_task')
+                                    host='localhost',
+                                    database='job_task')
 
         if cnx:
             print('Python MySQL Connection Established!')
-
-        cursor = cnx.cursor()
+            cursor = cnx.cursor()
 
     except Exception as err:
         print(err)
 
+    else:
+        read_everything('customers')
 
-    read_everything('customers')
+        # Adding 1 Million Dummy Data
+        # for i in range(1000000):
+        #     insert_data('fname', 'lname', f"fname@lname{i}.com")
 
-    cnx.commit()
-    cursor.close()
-    cnx.close()
+        cnx.commit()
+
+    finally:
+        cursor.close()
+        cnx.close()
